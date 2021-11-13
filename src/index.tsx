66f9bd680bb60f84b1123499392a8a7439307320
Dima Kelasev <dimakelaseff@gmail.com>
2021-11-13 14:30:11 +0300

    add redux-persist

--- src/index.tsx
+++ src/index.tsx
@@ -6,13 +6,26 @@ import { Provider } from "react-redux";
 import { rootReducer } from "./redux/store";
 import { createStore } from "redux";
 import { composeWithDevTools } from "redux-devtools-extension";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import { PersistGate } from "redux-persist/integration/react";
 
-const store = createStore(rootReducer, composeWithDevTools());
+const persistConfig = {
+  key: "TodoList",
+  storage: storage,
+};
+const pReducer = persistReducer<any, any>(persistConfig, rootReducer);
+
+const store = createStore(pReducer, composeWithDevTools());
+
+const persistor = persistStore(store);
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <PersistGate persistor={persistor}>
+        <App />
+      </PersistGate>
     </Provider>
   </React.StrictMode>,
   document.getElementById("root")
