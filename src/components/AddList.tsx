7a11e662279ab01e746419ddd0993b25ce4be63f
Dima Kelasev <dimakelaseff@gmail.com>
2021-11-25 16:03:48 +0300

    add new List modal

--- src/components/AddList.tsx
+++ src/components/AddList.tsx
@@ -1,6 +1,14 @@
 import React from "react";
+import { useDispatch } from "react-redux";
 import { AddComponent } from "../styles";
 
 export function AddList(): JSX.Element {
-  return <AddComponent>+ Add another List</AddComponent>;
+  const dispatch = useDispatch();
+
+  const openListModal = () => {
+    dispatch({ type: "OPEN_NEW_LIST_MODAL" });
+  };
+  return (
+    <AddComponent onClick={openListModal}>+ Add another List</AddComponent>
+  );
 }
